b81f5209c6b6d78ab8d3e0207a1b103ffb798fa6
Tanguy Genthon <tanguy.genthon22@gmail.com>
2024-08-12 10:24:40 +0200

    Add a plot to sanity checks

--- Sanity_checks.R
+++ Sanity_checks.R
@@ -1,31 +1,42 @@
 library(StrathE2E2)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(patchwork)
+
 
 rm(list=ls())
 
-model10 <- e2e_read("Norwegian_Basin", "2010-2019-ssp370", models.path="..")
-model20 <- e2e_read("Norwegian_Basin", "2020-2029-ssp370", models.path="..")
-model30 <- e2e_read("Norwegian_Basin", "2030-2039-ssp370", models.path="..")
-model40 <- e2e_read("Norwegian_Basin", "2040-2049-ssp370", models.path="..")
+model <- e2e_read("Norwegian_Basin", "2010-2019-ssp370", models.path="..")
+res=e2e_run(model,nyear=50)
 
-
-HR<-c(0.50,1.00,1.25,1.50)
-name<-c("model10","model20","model30","model40")
-list_model<-list()
-for (i in name){
-  for (j in HR){
-    new_model<-get(i)
-    new_model$data$fleet.model$HRscale_vector_multiplier[1]<- j
-    harvest_ratio<-format(j,nsmall=2)
-    new_model$setup$model.ident<-paste0(i,"_HR",harvest_ratio)
-    assign(paste0(i,"_HR",harvest_ratio),new_model)
-  }
+elt<-function(var,name){
+  return(var[[name]])
 }
-rm(model10,model20,model30,model40)
-names_model <- ls(pattern="^model")
+C_to_chl<-20   # Assumed carbon to chlorophyll ratio
+data		<- elt(model, "data")
+out<-elt(res,"output")
+physical.parms	<- elt(data, "physical.parameters")
+chemistry.drivers	<- elt(data, "chemistry.drivers")
+si_phyt<-elt(out, "phyt_si")
+so_phyt<-elt(out, "phyt_so")
+si_depth	<- elt(physical.parms, "si_depth")
+so_depth	<- elt(physical.parms, "so_depth")
 
+xvolume_si<-si_depth*x_shallowprop
+xvolume_so<-so_depth*(1-x_shallowprop)
+final_s_phyt<-data.frame(rep(0,12))
 
-for (i in names_model){
-  model<-get(i)
-  new_res<-e2e_run(model,nyear=10)
-  assign(gsub("model", "res_", i), new_res)
+for(mon in 1:12){
+final_s_phyt[mon,1]<-mean(((((si_phyt[((50-1)*360+((mon-1)*30)+1):((50-1)*360+(mon*30))]))*12*106)/16)/C_to_chl)/xvolume_si
+final_s_phyt[mon,2]<-mean(((((so_phyt[((50-1)*360+((mon-1)*30)+1):((50-1)*360+(mon*30))]))*12*106)/16)/C_to_chl)/xvolume_so
+final_s_phyt[mon,3]<-mon
 }
+colnames(final_s_phyt)<-c("Inshore","Offshore","Months")
+final_s_phyt<-final_s_phyt%>%
+  pivot_longer(cols=Inshore:Offshore)
+
+ggplot(final_s_phyt,aes(x=Months,y=value,color=name))+
+  geom_point(shape=16,size=4)+
+  scale_x_continuous(breaks = seq(1, 12), labels = month.name)+
+  theme_minimal()
